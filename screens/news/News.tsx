db6a2bdb450b56265b63cc33a42be3f4076154f2
strevo199 <mgbeojikwestephen@gmail.com>
2022-12-31 16:42:30 +0100

    done

--- screens/news/News.tsx
+++ screens/news/News.tsx
@@ -7,9 +7,13 @@ import {
   TextInput,
   FlatList,
   Alert,
+  TouchableOpacity,
+  Linking,
+  ActivityIndicator,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Animated, { FadeInUp } from 'react-native-reanimated';
 
 type NewsListInterface = {
   source: {
@@ -26,13 +30,14 @@ type NewsListInterface = {
 };
 
 export const News = () => {
+  const [isLoading, setisLoading] = useState(false)
   const [newsList, setnewsList] = useState<NewsListInterface[]>([]);
   const API_KEY = '73bfa4c7bc0b4cd4a5340aa39434d0ac';
   const newsApiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2022-11-30&sortBy=publishedAt&apiKey=${API_KEY}`;
   const RenderSearch = () => {
     return (
       <View>
-        <Text style={{fontSize: 24, fontWeight: 'bold'}}>Search</Text>
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Search</Text>
 
         <TextInput
           placeholder="Search by user or places"
@@ -43,6 +48,7 @@ export const News = () => {
             fontSize: 16,
             fontWeight: '400',
             borderWidth: 3,
+            backgroundColor:"white",
             borderColor: '#ddd',
             borderRadius: 10,
           }}
@@ -52,16 +58,24 @@ export const News = () => {
   };
 
   const getNews = () => {
+    setisLoading(true)
     axios
       .get(newsApiUrl)
       .then(res => {
+        setisLoading(false)
+
         if (res.data?.status === 'ok') {
           setnewsList(res.data.articles);
-        }
+        } 
+       
       })
       .catch(err => {
+        setisLoading(false)
+
         if (err) {
-          Alert.alert('Error', 'Unable to compelete request ');
+          Alert.alert('Error', 'Unable to compelete request ',[
+            {"text": "Refresh","onPress": ()=> getNews()}
+          ]);
         }
       });
   };
@@ -72,46 +86,89 @@ export const News = () => {
 
   const renderItem = (item: NewsListInterface) => {
     return (
-      <View
+      <Animated.View
+        entering={FadeInUp.delay(200).duration(500)}
         style={{
           borderColor: '#ddd',
           flexDirection: 'row',
           borderWidth: 0.7,
-          height: 100,
-          marginVertical: 8,
+          height: 110,
+          marginVertical: 6,
           borderRadius: 8,
+          backgroundColor:"white",
+          elevation:1,
+          shadowColor:"#000000",
+          shadowOffset: {
+            width: 1,
+            height:1
+          },
+          shadowRadius: 1,
+          shadowOpacity:0.3
+          
         }}>
         <View
           style={{
             height: '100%',
-            backgroundColor: 'black',
             width: 80,
+            
+            justifyContent:"center",
+            alignItems:"center"
           }}>
-          {!!item.urlToImage && (
             <Image
-              source={{url: item.urlToImage}}
+            
+              source={{uri: item.urlToImage}}
+              resizeMode="cover"
               style={{
-                height: '100%',
-                width: '50%',
+                height: 70,
+                borderRadius:35,
+                width: 70,
               }}
             />
-          )}
         </View>
-        <View style={{flex: 1, backgroundColor: 'red'}}>
-          <Text style={{color: 'black'}}>{item.title}</Text>
+        <View style={{flex: 1, padding:8, justifyContent:"space-between"}}>
+          <View>
+
+          <Text style={{color: 'black',marginBottom:4, textTransform:"capitalize", fontWeight:"500"}}>Author: {item.author ? `${item.author.substring(0,39)}`: "Stephen G"}</Text>
+          <Text style={{color: '#999999', fontWeight:"600"}}>Title: {item.title.substring(0,70)}..</Text>
+          </View>
         </View>
-      </View>
+          <View style ={{alignItems:"flex-end", marginBottom:2, marginRight:2}}>
+            <TouchableOpacity 
+              onPress={() =>Linking.openURL(item.url)}
+            style ={{height:25, width:75, 
+            borderRadius: 4, 
+            shadowColor:"#000000",
+          shadowOffset: {
+            width: 1,
+            height:1
+          },
+          shadowRadius: 1,bottom:0,
+          position:"absolute",
+          shadowOpacity:0.3,
+          zIndex:10,
+              borderWidth:0.3, elevation:2, backgroundColor:"white",
+               borderColor:"lightblue", justifyContent:"center", alignItems: "center"}}>
+              <Text style ={{fontSize:13, fontWeight:"500" }}>Read More</Text>
+            </TouchableOpacity>
+          </View>
+      </Animated.View>
     );
   };
 
   return (
-    <SafeAreaView style={{flex: 1, margin: 16}}>
-      <FlatList
+    <SafeAreaView style={{flex: 1, margin: 16, marginTop:30}}>
+
+      <RenderSearch/>
+
+      { isLoading ? <View style ={{flex:1, justifyContent: 'center', alignItems:"center"}}>
+        <ActivityIndicator size={"large"}/> 
+      </View>: <FlatList
         showsVerticalScrollIndicator={false}
         data={newsList}
-        ListHeaderComponent={RenderSearch}
+        ListHeaderComponent={<View><Text style={{fontSize: 18, fontWeight: 'bold'}}>Lastest News</Text></View>}
         renderItem={({item}) => renderItem(item)}
-      />
+       
+      />}
     </SafeAreaView>
   );
 };
